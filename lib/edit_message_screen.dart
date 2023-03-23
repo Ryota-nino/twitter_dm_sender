864f17ed6cec94ea409d494000010b152d723665
Ryota-nino <nino.brenpy@gmail.com>
2023-03-24 01:10:35 +0900

    `change` Widget名変更

--- lib/edit_message_screen.dart
+++ lib/edit_message_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class RegistMessageScreen extends StatelessWidget {
-  const RegistMessageScreen({
+class EditMessageScreen extends StatelessWidget {
+  const EditMessageScreen({
     required this.title,
     super.key,
   });
@@ -25,26 +25,6 @@ class RegistMessageScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 TextFormField(
-                  keyboardType: TextInputType.multiline,
-                  decoration: InputDecoration(
-                    labelText: 'タイトル',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(0),
-                      borderSide: const BorderSide(
-                        width: 2,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(0),
-                      borderSide: const BorderSide(
-                        width: 2,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 30),
-                TextFormField(
                   maxLines: 6,
                   minLines: 6,
                   keyboardType: TextInputType.multiline,
@@ -66,11 +46,34 @@ class RegistMessageScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(height: 30),
+                Container(
+                  alignment: Alignment.center,
+                  child: TextFormField(
+                    maxLength: 3,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: '送信する人数',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        borderSide: const BorderSide(
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  ),
+                ),
+                const SizedBox(height: 30),
                 ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("追加"),
+                  onPressed: () {},
+                  child: const Text("更新"),
                 )
               ],
             ),
