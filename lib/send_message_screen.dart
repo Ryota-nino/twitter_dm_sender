acdeba2aa756cf3043d1cd713d04f5f20fedc844
Ryota-nino <nino.brenpy@gmail.com>
2023-03-26 00:58:04 +0900

    `change` TextField外をタッチするとフォーカスが外れるように変更

--- lib/send_message_screen.dart
+++ lib/send_message_screen.dart
@@ -41,42 +41,46 @@ class SendMessageScreen extends StatelessWidget {
         ),
         icon: const Icon(Icons.send),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("アカウント名：リヴォルエッグ"),
-            const Text("ID：@rivolegg"),
-            Text(title),
-            Text(content),
-            const SizedBox(height: 30),
-            Text("このメッセージが未送信のフォロワー：3528名"),
-            const SizedBox(height: 30),
-            Container(
-              alignment: Alignment.center,
-              child: TextFormField(
-                maxLength: 3,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: '送信する人数',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(0),
-                    borderSide: const BorderSide(
-                      width: 2,
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        behavior: HitTestBehavior.opaque,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("アカウント名：リヴォルエッグ"),
+              const Text("ID：@rivolegg"),
+              Text(title),
+              Text(content),
+              const SizedBox(height: 30),
+              Text("このメッセージが未送信のフォロワー：3528名"),
+              const SizedBox(height: 30),
+              Container(
+                alignment: Alignment.center,
+                child: TextFormField(
+                  maxLength: 3,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: '送信する人数',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: const BorderSide(
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: const BorderSide(
+                        width: 2,
+                        color: Colors.blue,
+                      ),
                     ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(0),
-                    borderSide: const BorderSide(
-                      width: 2,
-                      color: Colors.blue,
-                    ),
-                  ),
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 ),
-                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
